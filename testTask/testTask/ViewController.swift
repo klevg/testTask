da888a9cbfc3f783d2acb4d4e2b2e804c67919f9
Евгений Клебан <kleban_eugene@tut.by>
2019-03-05 11:42:18 +0300

    initial commit

---
+++ testTask/testTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by Евгений Клебан on 3/5/19.
+//  Copyright © 2019 Евгений Клебан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
